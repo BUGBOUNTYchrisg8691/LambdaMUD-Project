77df92f375615d430d1d1e9dd4034d0207d03464
Christopher Girvin <chrisg8691@gmail.com>
2021-04-29 11:49:56 -0400

    testing say

--- adventure/api.py
+++ adventure/api.py
@@ -11,7 +11,13 @@ from rest_framework.decorators import api_view
 from .models import *
 
 # instantiate pusher
-pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
+pusher = Pusher(
+    app_id=config("PUSHER_APP_ID"),
+    key=config("PUSHER_KEY"),
+    secret=config("PUSHER_SECRET"),
+    cluster=config("PUSHER_CLUSTER"),
+)
+
 
 @csrf_exempt
 @api_view(["GET"])
@@ -22,19 +28,28 @@ def initialize(request):
     uuid = player.uuid
     room = player.room()
     players = room.playerNames(player_id)
-    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
+    return JsonResponse(
+        {
+            "uuid": uuid,
+            "name": player.user.username,
+            "title": room.title,
+            "description": room.description,
+            "players": players,
+        },
+        safe=True,
+    )
 
 
 # @csrf_exempt
 @api_view(["POST"])
 def move(request):
-    dirs={"n": "north", "s": "south", "e": "east", "w": "west"}
+    dirs = {"n": "north", "s": "south", "e": "east", "w": "west"}
     reverse_dirs = {"n": "south", "s": "north", "e": "west", "w": "east"}
     player = request.user.player
     player_id = player.id
     player_uuid = player.uuid
     data = json.loads(request.body)
-    direction = data['direction']
+    direction = data["direction"]
     room = player.room()
     nextRoomID = None
     if direction == "n":
@@ -47,19 +62,47 @@ def move(request):
         nextRoomID = room.w_to
     if nextRoomID is not None and nextRoomID > 0:
         nextRoom = Room.objects.get(id=nextRoomID)
-        player.currentRoom=nextRoomID
+        player.currentRoom = nextRoomID
         player.save()
         players = nextRoom.playerNames(player_id)
         currentPlayerUUIDs = room.playerUUIDs(player_id)
         nextPlayerUUIDs = nextRoom.playerUUIDs(player_id)
         for p_uuid in currentPlayerUUIDs:
-            pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
+            pusher.trigger(
+                f"p-channel-{p_uuid}",
+                "broadcast",
+                {"message": f"{player.user.username} has walked {dirs[direction]}."},
+            )
         for p_uuid in nextPlayerUUIDs:
-            pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
-        return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
+            pusher.trigger(
+                f"p-channel-{p_uuid}",
+                "broadcast",
+                {
+                    "message": f"{player.user.username} has entered from the {reverse_dirs[direction]}."
+                },
+            )
+        return JsonResponse(
+            {
+                "name": player.user.username,
+                "title": nextRoom.title,
+                "description": nextRoom.description,
+                "players": players,
+                "error_msg": "",
+            },
+            safe=True,
+        )
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+        return JsonResponse(
+            {
+                "name": player.user.username,
+                "title": room.title,
+                "description": room.description,
+                "players": players,
+                "error_msg": "You cannot move that way.",
+            },
+            safe=True,
+        )
 
 
 @csrf_exempt
@@ -73,6 +116,12 @@ def say(request):
     player_uuid = player.uuid
     print(player_uuid)
     data = json.loads(request.body)
-    print(data)
-    pusher.trigger(f"p-channel-${player_uuid}", u"broadcast", {"message": "hello world"})
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    room = player.room()
+    currentPlayerUUIDs = room.playerUUIDs(player_id)
+    for p_uuid in currentPlayerUUIDs:
+        pusher.trigger(
+            f"p-channel-{p_uuid}",
+            "broadcast",
+            {"message": f"{player.name} says {data.message}"},
+        )
+    return JsonResponse({"test": "test"}, safe=True)
